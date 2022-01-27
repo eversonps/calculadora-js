248dc563401c8e30d30b439ab99071c4a5d2f4c5
Everson Pereira da Silva <eversonpereiraofc@gmail.com>
2022-01-27 11:14:08 -0300

    add classe calculadora e funcionalidades de data e hora

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,57 @@
+class CalcController{
+    constructor(){
+        this._locale = "pt-br"
+        this._currentDate
+        this._displayCalcEl = document.querySelector("#display")
+        this._dateEl = document.querySelector("#data")
+        this._timeEl = document.querySelector("#hora")
+        this.initialize
+
+    }
+
+    initialize(){
+        this.displayCalc = "0"
+        this.setDisplayDateTime()
+        setInterval(()=>{
+            this.setDisplayDateTime()
+        }, 1000)
+
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+    }
+
+    set displayCalc(calc){
+        this._displayCalcEl.innerHTML = calc
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML
+    }
+
+    set displayDate(date){
+        this._dateEl.innerHTML = date
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML
+    }
+
+    set displayTime(time){
+        this._timeEl.innerHTML = time
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML     
+    }
+
+    get currentDate() {
+        return new Date()
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor
+    }
+}
